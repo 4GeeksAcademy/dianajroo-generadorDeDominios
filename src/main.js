6d3bdcd7cfe3749e46165334bcc74a500991f928
DianaJRoo <149723555+DianaJRoo@users.noreply.github.com>
2024-01-29 15:41:42 +0000

    generador de dominio casi listo

---
+++ src/main.js
@@ -0,0 +1,31 @@
+let pronombres = ['los', 'tus', 'nuestros'];
+let adjetivos = ['mejores', 'grandes', 'atrevidos'];
+let sustantivos = ['lentes', 'sueños', 'retos'];
+let extensiones = ['.com', '.es'];
+
+function generarCombinaciones() {
+    let combinaciones = [];
+    for (let pronombre of pronombres) {
+        for (let adjetivo of adjetivos) {
+            for (let sustantivo of sustantivos) {
+                for (let extension of extensiones) {
+                    // Combinación estándar
+                    let nombreDominio = `${pronombre}${adjetivo}${sustantivo}${extension}`;
+                    combinaciones.push(nombreDominio);
+
+                    // condicional
+                    if (sustantivo === 'lentes') {
+                        let domainHack = `${pronombre}${adjetivo}${sustantivo.substring(0, sustantivo.length - 2)}${extension}`;
+                        combinaciones.push(domainHack);
+                    }
+                }
+            }
+        }
+    }
+    return combinaciones;
+}
+
+let todasCombinaciones = generarCombinaciones();
+todasCombinaciones.forEach((dominio, idx) => {
+    console.log(`${idx +1}. ${dominio}`);
+});
